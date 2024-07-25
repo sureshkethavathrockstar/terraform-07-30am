2894fbf45955da01e9f52ebdd9ae01710a46f407
sureshkethavathrockstar <kethavathsuresh357@gmail.com>
2024-07-25 16:39:57 +0530

    Update ec2.tf

--- day-3-custom-network/ec2.tf
+++ day-3-custom-network/ec2.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "dev" {
     key_name = var.key_name
     subnet_id = aws_subnet.dev.id
     associate_public_ip_address = true
-    security_groups = [ aws_security_group.dev.id ]
  
     tags = {
       Name = "dev_ec2"
